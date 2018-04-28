8960eec76030909f8a7c5aea373502fc90bfe807
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2018-04-28 10:45:51 +0200

    Added solution for AES vulnerability

--- DECALab1/src/main/java/analysis/exercise1/MisuseAnalysis.java
+++ DECALab1/src/main/java/analysis/exercise1/MisuseAnalysis.java
@@ -1,9 +1,12 @@
 package analysis.exercise1;
 
+import java.util.List;
+
 import analysis.AbstractAnalysis;
 import analysis.VulnerabilityReporter;
 import soot.Body;
 import soot.Unit;
+import soot.ValueBox;
 
 public class MisuseAnalysis extends AbstractAnalysis{
 	public MisuseAnalysis(Body body, VulnerabilityReporter reporter) {
@@ -12,7 +15,15 @@ public class MisuseAnalysis extends AbstractAnalysis{
 	
 	@Override
 	protected void flowThrough(Unit unit) {
+		List<ValueBox> useBoxes = unit.getUseBoxes();
+		
+		for(int i = 0; i < useBoxes.size(); i++) {
+			ValueBox box = useBoxes.get(i);
+			if(box.getValue().toString().equals("staticinvoke <javax.crypto.Cipher: javax.crypto.Cipher getInstance(java.lang.String)>(\"AES\")")) {
+				reporter.reportVulnerability(this.method.toString(), unit);
+			}
 
+		}
 
 	}
 }
