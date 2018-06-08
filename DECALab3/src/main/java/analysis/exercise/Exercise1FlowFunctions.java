a388480393314a66f273d708e95315c43a8344f5
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2018-06-08 10:21:27 +0200

    reformated with editorconfig

--- DECALab3/src/main/java/analysis/exercise/Exercise1FlowFunctions.java
+++ DECALab3/src/main/java/analysis/exercise/Exercise1FlowFunctions.java
@@ -20,160 +20,160 @@ import soot.jimple.Stmt;
 
 public class Exercise1FlowFunctions extends TaintAnalysisFlowFunctions {
 
-	private VulnerabilityReporter reporter;
+    private VulnerabilityReporter reporter;
 
-	public Exercise1FlowFunctions(VulnerabilityReporter reporter) {
-		this.reporter = reporter;
-	}
+    public Exercise1FlowFunctions(VulnerabilityReporter reporter) {
+        this.reporter = reporter;
+    }
 
-	@Override
-	public FlowFunction<DataFlowFact> getCallFlowFunction(Unit callSite, SootMethod callee) {
-		return new FlowFunction<DataFlowFact>() {
-			@Override
-			public Set<DataFlowFact> computeTargets(DataFlowFact fact) {
-				if (fact.equals(DataFlowFact.zero()))
-					return Collections.emptySet();
-				prettyPrint(callSite, fact);
-				Set<DataFlowFact> out = Sets.newHashSet();
-				if (!(callSite instanceof Stmt)) {
-					return out;
-				}
-				Stmt callSiteStmt = (Stmt) callSite;
-				// TODO: Implement Exercise 1c) here
-				return out;
-			}
-		};
-	}
+    @Override
+    public FlowFunction<DataFlowFact> getCallFlowFunction(Unit callSite, SootMethod callee) {
+        return new FlowFunction<DataFlowFact>() {
+            @Override
+            public Set<DataFlowFact> computeTargets(DataFlowFact fact) {
+                if (fact.equals(DataFlowFact.zero()))
+                    return Collections.emptySet();
+                prettyPrint(callSite, fact);
+                Set<DataFlowFact> out = Sets.newHashSet();
+                if (!(callSite instanceof Stmt)) {
+                    return out;
+                }
+                Stmt callSiteStmt = (Stmt) callSite;
+                // TODO: Implement Exercise 1c) here
+                return out;
+            }
+        };
+    }
 
-	public FlowFunction<DataFlowFact> getCallToReturnFlowFunction(final Unit call, Unit returnSite) {
-		return new FlowFunction<DataFlowFact>() {
+    public FlowFunction<DataFlowFact> getCallToReturnFlowFunction(final Unit call, Unit returnSite) {
+        return new FlowFunction<DataFlowFact>() {
 
-			@Override
-			public Set<DataFlowFact> computeTargets(DataFlowFact val) {
+            @Override
+            public Set<DataFlowFact> computeTargets(DataFlowFact val) {
 
-				Set<DataFlowFact> out = Sets.newHashSet();
-				Stmt callSiteStmt = (Stmt) call;
-				out.add(val);
-				modelStringOperations(val, out, callSiteStmt);
+                Set<DataFlowFact> out = Sets.newHashSet();
+                Stmt callSiteStmt = (Stmt) call;
+                out.add(val);
+                modelStringOperations(val, out, callSiteStmt);
 
-				if (val.equals(DataFlowFact.zero())) {
-					// TODO: Implement Exercise 1a) here
-					if (call instanceof AssignStmt) {
+                if (val.equals(DataFlowFact.zero())) {
+                    // TODO: Implement Exercise 1a) here
+                    if (call instanceof AssignStmt) {
 
-						if (call.toString().contains("getParameter(")) {
-							AssignStmt assign = (AssignStmt) call;
-							if (assign.getLeftOp() instanceof Local) {
-								Local local = (Local) assign.getLeftOp();
-								out.add(new DataFlowFact(local));
-							} else {
-								if (assign.getLeftOp() instanceof SootField) {
-									SootField ref = (SootField) assign.getLeftOp();
-									out.add(new DataFlowFact(ref));
-								}
-							}
-						}
-					}
-				}
-				if (call instanceof Stmt && call.toString().contains("executeQuery")) {
-					Stmt stmt = (Stmt) call;
-					Value arg = stmt.getInvokeExpr().getArg(0);
-					if (val.getVariable().equals(arg)) {
-						reporter.reportVulnerability();
-					}
-				}
-				return out;
-			}
-		};
-	}
+                        if (call.toString().contains("getParameter(")) {
+                            AssignStmt assign = (AssignStmt) call;
+                            if (assign.getLeftOp() instanceof Local) {
+                                Local local = (Local) assign.getLeftOp();
+                                out.add(new DataFlowFact(local));
+                            } else {
+                                if (assign.getLeftOp() instanceof SootField) {
+                                    SootField ref = (SootField) assign.getLeftOp();
+                                    out.add(new DataFlowFact(ref));
+                                }
+                            }
+                        }
+                    }
+                }
+                if (call instanceof Stmt && call.toString().contains("executeQuery")) {
+                    Stmt stmt = (Stmt) call;
+                    Value arg = stmt.getInvokeExpr().getArg(0);
+                    if (val.getVariable().equals(arg)) {
+                        reporter.reportVulnerability();
+                    }
+                }
+                return out;
+            }
+        };
+    }
 
-	private void modelStringOperations(DataFlowFact fact, Set<DataFlowFact> out, Stmt callSiteStmt) {
-		if (callSiteStmt instanceof AssignStmt && callSiteStmt.toString().contains("java.lang.StringBuilder append(")
-				&& callSiteStmt.getInvokeExpr() instanceof InstanceInvokeExpr) {
-			Value arg0 = callSiteStmt.getInvokeExpr().getArg(0);
-			Value base = ((InstanceInvokeExpr) callSiteStmt.getInvokeExpr()).getBase();
-			/*
-			 * Does the propagated value match the first parameter of the append call or the
-			 * base variable
-			 */
-			if (fact.getVariable().equals(arg0) || fact.getVariable().equals(base)) {
-				/* Yes, then taint the left side of the assignment */
-				Value leftOp = ((AssignStmt) callSiteStmt).getLeftOp();
-				if (leftOp instanceof Local) {
-					out.add(new DataFlowFact((Local) leftOp));
-				}
-			}
-		}
+    private void modelStringOperations(DataFlowFact fact, Set<DataFlowFact> out, Stmt callSiteStmt) {
+        if (callSiteStmt instanceof AssignStmt && callSiteStmt.toString().contains("java.lang.StringBuilder append(")
+                && callSiteStmt.getInvokeExpr() instanceof InstanceInvokeExpr) {
+            Value arg0 = callSiteStmt.getInvokeExpr().getArg(0);
+            Value base = ((InstanceInvokeExpr) callSiteStmt.getInvokeExpr()).getBase();
+            /*
+             * Does the propagated value match the first parameter of the append call or the
+             * base variable
+             */
+            if (fact.getVariable().equals(arg0) || fact.getVariable().equals(base)) {
+                /* Yes, then taint the left side of the assignment */
+                Value leftOp = ((AssignStmt) callSiteStmt).getLeftOp();
+                if (leftOp instanceof Local) {
+                    out.add(new DataFlowFact((Local) leftOp));
+                }
+            }
+        }
 
-		/*
-		 * For any call x = var.toString(), if the base variable var is tainted, then x
-		 * is tainted.
-		 */
-		if (callSiteStmt instanceof AssignStmt && callSiteStmt.toString().contains("toString()")) {
-			if (callSiteStmt.getInvokeExpr() instanceof InstanceInvokeExpr) {
-				InstanceInvokeExpr instanceInvokeExpr = (InstanceInvokeExpr) callSiteStmt.getInvokeExpr();
-				if (fact.getVariable().equals(instanceInvokeExpr.getBase())) {
-					Value leftOp = ((AssignStmt) callSiteStmt).getLeftOp();
-					if (leftOp instanceof Local) {
-						out.add(new DataFlowFact((Local) leftOp));
-					}
-				}
-			}
-		}
-	}
+        /*
+         * For any call x = var.toString(), if the base variable var is tainted, then x
+         * is tainted.
+         */
+        if (callSiteStmt instanceof AssignStmt && callSiteStmt.toString().contains("toString()")) {
+            if (callSiteStmt.getInvokeExpr() instanceof InstanceInvokeExpr) {
+                InstanceInvokeExpr instanceInvokeExpr = (InstanceInvokeExpr) callSiteStmt.getInvokeExpr();
+                if (fact.getVariable().equals(instanceInvokeExpr.getBase())) {
+                    Value leftOp = ((AssignStmt) callSiteStmt).getLeftOp();
+                    if (leftOp instanceof Local) {
+                        out.add(new DataFlowFact((Local) leftOp));
+                    }
+                }
+            }
+        }
+    }
 
-	@Override
-	public FlowFunction<DataFlowFact> getNormalFlowFunction(final Unit curr, Unit succ) {
-		return new FlowFunction<DataFlowFact>() {
-			@Override
-			public Set<DataFlowFact> computeTargets(DataFlowFact fact) {
-				prettyPrint(curr, fact);
-				Set<DataFlowFact> out = Sets.newHashSet();
-				out.add(fact);
-				// TODO: Implement Exercise 1b) here
-				if (curr instanceof AssignStmt) {
-					
-					AssignStmt ass = (AssignStmt) curr;
-					DataFlowFact rightVariable = null;
-					if (ass.getRightOp() instanceof SootField) {
-						SootField rightField = (SootField) ass.getRightOp();
-						rightVariable = new DataFlowFact(rightField);
-					} else {
-						if (ass.getRightOp() instanceof Local) {
-							Local rightLocal = (Local) ass.getRightOp();
-							rightVariable = new DataFlowFact(rightLocal);
-						}
-					}
-					
-					if (out.contains(rightVariable)) {
-						
-						DataFlowFact leftVariable = null;
-						if (ass.getLeftOp() instanceof SootField) {
-							SootField leftField = (SootField) ass.getLeftOp();
-							leftVariable = new DataFlowFact(leftField);
-						} else {
-							if (ass.getLeftOp() instanceof Local) {
-								Local leftLocal = (Local) ass.getLeftOp();
-								leftVariable = new DataFlowFact(leftLocal);
-							}
-						}
-						
-						out.add(leftVariable);
-					}
-				}
-				return out;
-			}
-		};
-	}
+    @Override
+    public FlowFunction<DataFlowFact> getNormalFlowFunction(final Unit curr, Unit succ) {
+        return new FlowFunction<DataFlowFact>() {
+            @Override
+            public Set<DataFlowFact> computeTargets(DataFlowFact fact) {
+                prettyPrint(curr, fact);
+                Set<DataFlowFact> out = Sets.newHashSet();
+                out.add(fact);
+                // TODO: Implement Exercise 1b) here
+                if (curr instanceof AssignStmt) {
 
-	@Override
-	public FlowFunction<DataFlowFact> getReturnFlowFunction(Unit callSite, SootMethod callee, Unit exitStmt,
-			Unit retSite) {
-		return new FlowFunction<DataFlowFact>() {
-			@Override
-			public Set<DataFlowFact> computeTargets(DataFlowFact fact) {
-				prettyPrint(callSite, fact);
-				return Collections.emptySet();
-			}
-		};
-	}
+                    AssignStmt ass = (AssignStmt) curr;
+                    DataFlowFact rightVariable = null;
+                    if (ass.getRightOp() instanceof SootField) {
+                        SootField rightField = (SootField) ass.getRightOp();
+                        rightVariable = new DataFlowFact(rightField);
+                    } else {
+                        if (ass.getRightOp() instanceof Local) {
+                            Local rightLocal = (Local) ass.getRightOp();
+                            rightVariable = new DataFlowFact(rightLocal);
+                        }
+                    }
+
+                    if (out.contains(rightVariable)) {
+
+                        DataFlowFact leftVariable = null;
+                        if (ass.getLeftOp() instanceof SootField) {
+                            SootField leftField = (SootField) ass.getLeftOp();
+                            leftVariable = new DataFlowFact(leftField);
+                        } else {
+                            if (ass.getLeftOp() instanceof Local) {
+                                Local leftLocal = (Local) ass.getLeftOp();
+                                leftVariable = new DataFlowFact(leftLocal);
+                            }
+                        }
+
+                        out.add(leftVariable);
+                    }
+                }
+                return out;
+            }
+        };
+    }
+
+    @Override
+    public FlowFunction<DataFlowFact> getReturnFlowFunction(Unit callSite, SootMethod callee, Unit exitStmt,
+            Unit retSite) {
+        return new FlowFunction<DataFlowFact>() {
+            @Override
+            public Set<DataFlowFact> computeTargets(DataFlowFact fact) {
+                prettyPrint(callSite, fact);
+                return Collections.emptySet();
+            }
+        };
+    }
 }
